a5b6e1236fc6912bd7db80318483e51f039c43dc
gayathrisai96 <50397493+gayathrisai96@users.noreply.github.com>
2019-05-08 15:05:15 +0530

    Update supriya.c

--- supriya.c
+++ supriya.c
@@ -6,15 +6,10 @@ void main()
     int secondNumber=10;
     int sumOfTwoNumbers;
     
-    //printf("Enter two integers: ");
-
-    // Two integers entered by user is stored using scanf() function
-    //scanf("%d %d", &firstNumber, &secondNumber);
-
-    // sum of two numbers in stored in variable sumOfTwoNumbers
+    
+    
     sumOfTwoNumbers = firstNumber + secondNumber;
-
-    // Displays sum      
+      
     printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
 
     
